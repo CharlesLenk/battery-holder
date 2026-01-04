f05ee4b6f60eb3052f7dd13594e36bd5de75c24f
Charles Lenk <lenk.charles@gmail.com>
2026-01-04 12:18:09 -0800

    Added a parameter to adjust the number of columns

--- export map.scad
+++ export map.scad
@@ -2,8 +2,9 @@ include <openscad-utilities/common.scad>
 use <battery holder.scad>
 
 name = "";
+columns = 6;
 
 if (name == "holder")
-    rotate([180, 0, 0]) holder();
+    rotate([180, 0, 0]) holder(columns);
 else if (name == "symbol_insert")
     rotate([180, 0, 0]) symbol_insert();
--- export.py
+++ export.py
@@ -1,10 +1,9 @@
-from scad_export.export import export
-from scad_export.exportable import Folder, Model
+from scad_export.export import Folder, Model, export
 
 files=Folder(
     name='scad_export/battery_holder',
     contents=[
-        Model(name='holder'),
+        [Model(name='holder', columns=columns) for columns in range(4, 9, 2)],
         Model(name='symbol_insert')
     ]
 )
